79e4652df271c4f93eb40ae0d6426bc27eb8c682
grigorijtomczuk <grigorijtomczuk@gmail.com>
2023-02-21 16:44:56 +0300

    Define scroll y offset using viewport height relative unit

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,8 +2,11 @@ import { useEffect } from "react";
 
 // A custom scroll function to pass in the "scroll" property of <HashLink> elements
 export function scrollWithOffset(element) {
+	// Define viewport units
+	// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+	const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 	const yCoordinate = element.getBoundingClientRect().top + window.scrollY;
-	const yOffset = -150;
+	const yOffset = -vh / 6;
 	window.scrollTo({ top: yCoordinate + yOffset, behavior: "smooth" });
 }
 
